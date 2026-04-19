45a8bc539a49a3131bb3ed7c474eb552e2938be9
Joseph Fuentes <josephfv2812@gmail.com>
2026-04-19 23:13:41 +0200

    Agregando componente Search, pagina principal y layout correspondiente

---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+import Search from '../components/searchInput'
+
+export default function HomePage(){
+    return (
+        <Search/>
+    )
+}
---
+++ components/searchInput.jsx
@@ -0,0 +1,8 @@
+export default function searchInput(){
+    return (
+        <form action="">
+            <input type="text" placeholder="Que desea buscar?"/>
+            <button type="submit">Buscar</button>
+        </form>
+    )
+}
